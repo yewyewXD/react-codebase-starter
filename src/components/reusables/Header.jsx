d8b8d3ec1677c0b464ff10a902cd04a16b234830
Yew Kang Wei <yewyew6933@gmail.com>
2021-02-28 11:34:30 +0800

    add header routes and scss selector

--- src/components/reusables/Header.jsx
+++ src/components/reusables/Header.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <header className="Header">a global used header</header>;
+  const routes = useMemo(
+    () => [
+      {
+        name: "Home",
+        link: "/home",
+      },
+      {
+        name: "Page One",
+        link: "/page1",
+      },
+    ],
+    []
+  );
+
+  return (
+    <header className="Header">
+      {routes.map((route, index) => (
+        <Link className="NavLink" to={route.link} key={`route-${index}`}>
+          {route.name}
+        </Link>
+      ))}
+    </header>
+  );
 };
 
 export default Header;
