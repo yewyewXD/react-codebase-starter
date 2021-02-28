e99f7801cd8cfa33d4096c99e83d7344b59c3af1
Yew Kang Wei <yewyew6933@gmail.com>
2021-02-28 10:57:58 +0800

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./mainPages/Home";
+import PageOne from "./mainPages/PageOne";
+
 function App() {
-  return <div>ok</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/page1" component={PageOne} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/mainPages/PageOne.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PageOne = () => {
+  return <div>this is page one</div>;
+};
+
+export default PageOne;
